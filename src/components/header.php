bb34a1a0356e59c2e8803e9ea0bf2335d89f6e7a
Sergio Humberto Guzmán <sergiohumberto27@gmail.com>
2019-10-17 01:42:54 -0600

    Pagina  de reseña de peliculas

---
+++ src/components/header.php
@@ -0,0 +1,25 @@
+<header class="site-header">
+  <div class="container">
+    <a href="../index.php" id="branding">
+      <img src="images/logo.png" alt="" class="logo">
+      <div class="logo-copy">
+        <h1 class="site-title">Pagina de Peliculas</h1>
+        <small class="site-description">Reseña de peliculas</small>
+      </div>
+    </a> <!-- #branding -->
+
+    <div class="main-navigation">
+      <button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
+      <ul class="menu">
+        <li class="menu-item current-menu-item"><a href="../index.php">Home</a></li>
+        <li class="menu-item"><a href="#">About</a></li>
+        <li class="menu-item"><a href="#">Movie reviews</a></li>
+        <li class="menu-item"><a href="#">Join us</a></li>
+        <li class="menu-item"><a href="#">Contact</a></li>
+      </ul> <!-- .menu -->
+
+    </div> <!-- .main-navigation -->
+
+    <div class="mobile-navigation"></div>
+  </div>
+</header>
---
+++ src/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+
+  <title>Pagina de Peliculas</title>
+
+  <!-- Loading third party fonts -->
+  <link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
+  <link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Loading main css file -->
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<head>
+  <div id="site-content">
+    <?php include "components/header.php"?>
+    <?php include "pages/home.php" ?>
+    <?php include "components/footer.php" ?>
+  </div>
+  <script src="../js/jquery-1.11.1.min.js"></script>
+  <script src="../js/plugins.js"></script>
+  <script src="../js/app.js"></script>
+</head>
